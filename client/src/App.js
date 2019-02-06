aa9afd214f0f8da20a3007910d20eb309b381140
Runar <Runar@tuta.io>
2019-02-06 13:55:43 +0100

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,45 @@ import React, {
 } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Link,
+  Route
+} from 'react-router-dom';
+import { Grommet, Box } from 'grommet';
+
+const Homepage = () => (
+  <Box>
+    <a href="/auth/google">
+      Sign in to Google{' '}
+    </a>
+    <Link to="/about/">About</Link>
+  </Box>
+);
+
+const About = () => (
+  <Box>
+    About me
+    <Link to="/">Home</Link>
+  </Box>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <a href="/auth/google">
-          Sign in to Google{' '}
-        </a>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route
+            path="/"
+            exact
+            component={Homepage}
+          />
+          <Route
+            path="/about/"
+            component={About}
+          />
+        </div>
+      </BrowserRouter>
     );
   }
 }
