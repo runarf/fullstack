072ef82bf163ff6c071399e3144c778ac8c1bf82
Runar <Runar@tuta.io>
2019-02-06 13:29:52 +0100

    added client

--- index.js
+++ index.js
@@ -23,5 +23,29 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if (
+  process.env.NODE_ENV ===
+    'production' ||
+  true
+) {
+  app.use(
+    express.static('client/build')
+  );
+
+  console.log('in server');
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        'client',
+        'build',
+        'index.html'
+      )
+    );
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
